a04a1468d8e05e3e3ae801e500536be3197bbc26
shibili66 <shibilivesala26@gmail.com>
2022-09-16 16:14:07 +0530

    commit

---
+++ bin/vsproject.dart
@@ -0,0 +1,5 @@
+import 'package:vsproject/vsproject.dart' as vsproject;
+
+void main(List<String> arguments) {
+  print('Hello world: ${vsproject.calculate()}!');
+}
---
+++ test/vsproject_test.dart
@@ -0,0 +1,8 @@
+import 'package:vsproject/vsproject.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
