b9bbdf443b9f5c1bfeae3fae9b4530c68a821573
shibili66 <shibilivesala26@gmail.com>
2022-09-19 16:05:51 +0530

    commit

--- bin/vsproject.dart
+++ bin/vsproject.dart
@@ -1,5 +1,18 @@
-import 'package:vsproject/vsproject.dart' as vsproject;
 
-void main(List<String> arguments) {
-  print('Hello world: ${vsproject.calculate()}!');
+import 'dart:io';
+
+
+void main(){
+
+  String name = 'shibili';
+  int age = 20;
+  double mark = 9.9;
+  String email = "shibili@gmail.com";
+  int no = 7902497266;
+
+  print("my name is $name");
+  print("i am $age years old");
+  stdout.write("mark percentage is $mark");
+  stdout.write("email id is $email");
+
 }
